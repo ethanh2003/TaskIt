183369d272ed8631a6da08e595e88818cb90c6d0
Ethan Herring <echerring.ech@gmail.com>
2023-08-04 15:04:54 -0400

    initial

---
+++ src/main/java/com/example/taskit/TaskItApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taskit/TaskItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
