fea90098a34b4e62684630a83e8a7d9ee64a4d4c
Ethan Herring <echerring.ech@gmail.com>
2023-08-04 20:51:24 -0400

    added todo for issue

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -13,6 +13,33 @@ const TaskList = () => {
             .catch(error => console.error(error));
     }, []);
 
+    const deleteTask = (id) => {
+        axios.delete(`/api/tasks/${id}`)
+            .then(response => {
+                console.log('Task deleted successfully:', id);
+                setTasks(tasks.filter(task => task.id !== id));
+            })
+            .catch(error => console.error(error));
+    };
+
+
+    const markAsCompleted = (id) => {
+        const updatedTasks = tasks.map(task => {
+            if (task.id === id) {
+                return { ...task, completed: true };
+            }
+            return task;
+        });
+
+        axios.put(`/api/tasks/${id}`, { completed: true })
+            .then(response => {
+                console.log('Task marked as completed:', response.data);
+                setTasks(updatedTasks);
+            })
+            .catch(error => console.error(error));
+    };
+
+
 
     return (
         <div>
@@ -21,8 +48,9 @@ const TaskList = () => {
                 {tasks.map(task => (
                     <li key={task.id}>
                         {task.title}
-                        <button>Mark as Completed</button>
-                        <button>Delete</button>
+                        {/*TODO Delete and complete do not work*/}
+                        <button onClick={() => markAsCompleted(task.id)}>Mark as Completed</button>
+                        <button onClick={() => deleteTask(task.id)}>Delete</button>
                         {/*TODO Links do not work*/}
                         <Link to={`/edit/${task.id}`}>Edit</Link>
                     </li>
@@ -30,6 +58,7 @@ const TaskList = () => {
             </ul>
             {/* Link to the AddTask component */}
             <Link to="/add">Add Task</Link>
+
         </div>
     );
 }
